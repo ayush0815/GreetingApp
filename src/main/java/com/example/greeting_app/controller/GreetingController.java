8c87b36b82a1474b0391a06e38ce2c0813201309
ayush0815 <predatorof09@gmail.com>
2025-03-03 11:34:19 +0530

    Implemented UC2: Added GreetingService

--- src/main/java/com/example/greeting_app/controller/GreetingController.java
+++ src/main/java/com/example/greeting_app/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.greeting_app.controller;
 
+import com.example.greeting_app.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    // Constructor-based Dependency Injection
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     @GetMapping("/message")
     public String getMessage() {
-        return "Hello, Welcome to the Greeting App!";
+        return greetingService.getGreetingMessage();
     }
 }
